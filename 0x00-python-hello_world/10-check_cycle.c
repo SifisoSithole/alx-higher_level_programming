083da9fe41f40708a44d1c22d638cd3a41f96919
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-08-22 17:53:41 +0200

    Add: 10-check_cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,22 +10,42 @@
  */
 int check_cycle(listint_t *list)
 {
-	int i = 0;
-
-	listint_t *h = list;
+	int begin = 0;
+	int len = 0, end = 0;
+	size_t size = sizeof(char *) * 180;
+	listint_t **address = NULL;
+	listint_t **tempAddr = NULL;
 
 	if (!list)
 		return (0);
+	address = malloc(size);
+	address[end] = list;
+	list = list->next;
 	while (list)
 	{
-		if (list == h)
-			return (1);
-		for (i = 0; i < 3; i++)
+		begin = 0;
+		end = len;
+		while (begin <= end)
 		{
-			if (h-next == NULL)
-				return(0);
-			h->next;
+			if (address[begin] == list || address[end] == list)
+			{
+				free(address);
+				return (1);
+			}
+			begin++;
+			end--;
 		}
+		if (sizeof(char *) * len > size)
+		{
+			tempAddr = malloc(size * 2);
+			size *= 2;
+			for (begin = 0; begin < len; begin++)
+				tempAddr[begin] = address[begin];
+			free(address);
+			address = tempAddr;
+		}
+		len++;
+		address[len] = list;
 		list = list->next;
 	}
 	free(address);
