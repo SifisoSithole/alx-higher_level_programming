c326fa7a20679a9606e453207ecbe1da6db7f558
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-08-23 13:08:40 +0200

    Add: 6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for first in range(0,9):
+for first in range(0, 9):
     for second in range(first + 1, 10):
         if first != 8:
             print("{0}{1}".format(first, second), end=", ")
