fcaa8a72f2ab28b762a4e02e2af12ccf06da3a48
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-08-23 16:23:06 +0200

    Add: 100-print_tebahpla.py

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+uppercase = False
+for i in range(122, 96, -1):
+    c = i;
+    if uppercase:
+        c -= 32
+        uppercase = False
+    else:
+        uppercase = True
+    print("{:c}".format(c), end='')
