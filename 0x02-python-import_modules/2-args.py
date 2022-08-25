6b30030b757ffde5e16407546d8144b366a7c19c
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-08-25 12:49:23 +0200

    Add: 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,9 +5,9 @@ if __name__ == "__main__":
     ac = len(sys.argv)
     if ac > 1:
         if ac == 1:
-            print(f"{ac:d} argument:")
+            print(f"{ac - 1:d} argument:")
         else:
-            print(f"{ac:d} arguments:")
+            print(f"{ac - 1:d} arguments:")
         for i in range(1, ac):
             print(f"{i:d}: {sys.argv[i]:s}")
     else:
