8953572440c9dbce6f0a02b57e5c746a466e09a6
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-08-25 12:34:32 +0200

    Add: 2-args.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import calculator_1 as cal
-a = 10
-b = 5
-signs = ["+", "-", "*", "/"]
-functions = [cal.add, cal.sub, cal.mul, cal.div]
-for i in range(len(functions)):
-    print("{0:d} {1:s} {2:d} = {3:d}".format(a, signs[i], b, functions[i](a, b)))
+if __name__ == "__main__":
+    import calculator_1 as cal
+    a = 10
+    b = 5
+    signs = ["+", "-", "*", "/"]
+    functions = [cal.add, cal.sub, cal.mul, cal.div]
+    for i in range(len(functions)):
+        print("{0:d} {1:s} {2:d} = {3:d}".format(a, signs[i], b, functions[i](a, b)))
