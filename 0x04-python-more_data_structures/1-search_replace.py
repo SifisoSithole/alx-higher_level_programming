f7a18925d652e234f42d40e85f9d02554504b255
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-08-31 15:07:41 +0200

    Add: 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    if my_list == None:
+        return None
     new_list = my_list.copy()
     if search in my_list:
         i = my_list.index(search)
