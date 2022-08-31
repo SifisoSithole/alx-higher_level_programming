6b2fd08631a464c984f64724bc7a90b286380d30
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-08-31 14:19:44 +0200

    Add: 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [[row[i] ** 2 for row in matrix] for i in range(len(matrix[0]))]
+    return [[matrix[y][x] ** 2 for x in range(len(matrix[0]))] for y in range(len(matrix))]
