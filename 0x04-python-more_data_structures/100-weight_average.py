317a3bfa0cb644e05a70f7ab19fc408ce4d132a4
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-08-31 19:04:03 +0200

    Add: 100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    total = 0
+    divisor = 0
+    for tup in my_list:
+        total += tup[0] * tup[1]
+        divisor += tup[1]
+    return total / divisor
