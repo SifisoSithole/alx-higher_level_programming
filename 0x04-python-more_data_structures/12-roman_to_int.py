62c9b41d39ad04b6f6242ae4889fa078c69f7279
Sifiso Sithole <sitholesifisobrian@gmail.com>
2022-08-31 18:30:38 +0200

    Add: 10-roman_to_int.py

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    se = {
+            "R": 0,
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+    res = 0
+    roman_string += 'R'
+    i = 0
+    while i < len(roman_string) - 1:
+        if se[roman_string[i]] < se[roman_string[i+1]]:
+            res += se[roman_string[i+1]] - se[roman_string[i]]
+            i += 2
+        else:
+            res += se[roman_string[i]]
+            i += 1
+    return res
