cb066a67d2bab8173efc96f1742211e121cff57b
SifisoSithole <sitholesifisobrian@gmail.com>
2022-09-12 14:42:29 +0200

    Add: 1-safe_print_integer.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    if x == None:
+        return 0
+    num = 0
     for i in range(x):
         try:
             print("{}".format(my_list[i]), end="")
         except IndexError:
             break
+        except Exception:
+            break
+        else:
+            num += 1
     print()
-    if i + 1 == x:
-        return i + 1
-    else:
-        return i
+    return num
