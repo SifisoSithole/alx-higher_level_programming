c9f901d8d4497e5282a1d9f32210f99847972157
SifisoSithole <sitholesifisobrian@gmail.com>
2022-09-13 14:04:36 +0200

    Add: 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,7 +10,7 @@ class Square:
         Args:
             size (int): The size of the square
         """
-        if type(size) is not int:
+        if type(size) is not int or size is None:
             raise TypeError("size must be a integer")
         if size < 0:
             raise ValueError("size must be >= 0")
