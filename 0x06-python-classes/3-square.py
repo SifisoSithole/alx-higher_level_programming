bdaedd39c30561a50c2252e4f3303ac28ffa39b0
SifisoSithole <sitholesifisobrian@gmail.com>
2022-09-13 14:14:06 +0200

    Add: 3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """A square represantation"""
+
+    def __init__(self, size=0):
+        """Create a square
+        Args:
+            size (int): The size of the square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return area of Square"""
+        return (self.__size ** 2)
