ef915c8fa28eefa67fa40690e1245cdc050fe6fd
SifisoSithole <sitholesifisobrian@gmail.com>
2022-09-13 14:01:23 +0200

    Add: 2-square.py

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """A square represantation"""
+
+    def __init__(self, size=0):
+        """Create a square
+        Args:
+            size (int): The size of the square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be a integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
