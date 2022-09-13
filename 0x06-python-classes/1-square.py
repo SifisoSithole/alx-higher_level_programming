abe458dbdb18c82e2eea0928dc613cb3c841138c
SifisoSithole <sitholesifisobrian@gmail.com>
2022-09-13 13:40:12 +0200

    Add: 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """A square represantation"""
+
+
+    def __init__(self, size):
+        """Create a square
+        Args:
+            size (int): The size of the square
+        """
+        self.__size = size
