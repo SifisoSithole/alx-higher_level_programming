8376171fef8a76deeca7c03c603548a5b4cd761e
SifisoSithole <sitholesifisobrian@gmail.com>
2022-09-13 14:24:31 +0200

    Add: 4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """A square represantation"""
+
+    def __init__(self, size=0):
+        """Create a square
+        Args:
+            size (int): The size of the square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Get the size of the square"""
+        return (self.__size)
+    @size.setter
+    def size(self, num):
+        """Set the size of the square"""
+        if type(num) is not int:
+            raise TypeError("size must be an integer")
+        if num < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = num
+
+    def area(self):
+        """Return area of Square"""
+        return (self.__size ** 2)
