7eabea93e947ccfa0f8e1fe1c660ddc71a09f41b
SifisoSithole <sitholesifisobrian@gmail.com>
2022-09-20 12:43:36 +0200

    Add: 19-copy_list.py

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,17 +1,3 @@
 #!/usr/bin/python3
-"""Returns a copy of a list"""
-
-
 def copy_list(l):
-    """
-    Returns a copy of a list
-    Parameteres:
-        l (list): List to copy
-    Return:
-        new_list (list): Copy of l
-    """
-    if isinstance(l, list):
-        new_list = l[:]
-        return new_list
-    else:
-        return None
+    return l[:]
