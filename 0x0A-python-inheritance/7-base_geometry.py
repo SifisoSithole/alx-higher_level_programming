591edde16ea73dcfae6933d264d825447753a747
SifisoSithole <sitholesifisobrian@gmail.com>
2022-09-26 16:03:29 +0200

    Add: 8-rectangle.py

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Define a BaseGeometry class"""
+
+
+class BaseGeometry:
+    """Super class"""
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates integers"""
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
