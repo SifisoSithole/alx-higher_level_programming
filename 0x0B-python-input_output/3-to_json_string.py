31bce5cbe2db7d8dd8431ac44a4448f2274660b6
SifisoSithole <sitholesifisobrian@gmail.com>
2022-09-27 13:57:31 +0200

    Add: 3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,16 @@
+import json
+"""
+3-to_json_string module
+
+Returns the JSON represantation of an object
+"""
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON represantation of an object
+    parameters:
+        my_object (object): Object to convert
+    Return:
+        String represantation of my_object
+    """
+    return json.dumps(my_obj)
