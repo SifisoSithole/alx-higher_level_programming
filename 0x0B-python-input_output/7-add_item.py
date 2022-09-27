7929f49a5e0e75004c4682a5c9c6e665894ca929
SifisoSithole <sitholesifisobrian@gmail.com>
2022-09-27 15:54:17 +0200

    Add: file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-This script that adds all arguments to a Python list, and then save them to a file
+This script that adds all arguments to a Python list,
+and then save them to a file
 """
 save_to_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_file = __import__("6-load_from_json_file").load_from_json_file
