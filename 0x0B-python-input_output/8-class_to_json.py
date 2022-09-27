f9f1d57584a070dc98ff453c98472892f51d914e
SifisoSithole <sitholesifisobrian@gmail.com>
2022-09-27 16:08:28 +0200

    Add: 8-my_class.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 returns the dictionary description with simple data structure
-(list, dictionary, string, integer and boolean) for JSON serialization 
+(list, dictionary, string, integer and boolean) for JSON serialization
 of an object
 """
 
