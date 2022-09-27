158dc71c681da017375bebefc134d0c71ac3824b
SifisoSithole <sitholesifisobrian@gmail.com>
2022-09-27 14:23:44 +0200

    Add: 4-from_json_string.py

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+4-from_json_string module
+
+Returns the JSON represantation of an object
+"""
+
+
+def from_json_string(my_str):
+    """
+    Returns the python data structure represanted by a JSON string
+    parameters:
+        my_str (str): 
+    Return:
+        Python data structure
+    """
+    json = __import__('json')
+    return json.loads(my_str)
