78e854c6c1a5e709e50495c3e55cea9e4f236d7c
SifisoSithole <sitholesifisobrian@gmail.com>
2022-11-02 12:44:17 +0200

    push: files

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -4,6 +4,7 @@ USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities
 (
 	id INT PRIMARY KEY NOT NULL UNIQUE AUTO_INCREMENT,
-	state_id INT NOT NULL FOREIGN KEY REFERENCES states(id),
-	name VARCHAR(256)
+	state_id INT NOT NULL,
+	name VARCHAR(256),
+	FOREIGN KEY (state_id) REFERENCES states(id)
 )
