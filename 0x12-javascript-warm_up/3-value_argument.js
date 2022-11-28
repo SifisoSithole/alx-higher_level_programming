7618f54a314b7aa3b5010203a0187384b3f2be1b
SifisoSithole <sitholesifisobrian@gmail.com>
2022-11-28 15:11:52 +0200

    push: 3-arguments.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
 const argv = require('node:process').argv;
-let i = 2;
 
 if (typeof (argv[2]) === 'undefined') {
   console.log('No argument');
 } else {
-  while (typeof (argv[i]) !== 'undefined') {
-    console.log(argv[i]);
-    i++;
-  }
+  console.log(argv[2]);
 }
