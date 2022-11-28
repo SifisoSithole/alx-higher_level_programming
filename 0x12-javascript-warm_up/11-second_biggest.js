ef58c4f397a7c8db0b8879ac130b4be40eeca75e
SifisoSithole <sitholesifisobrian@gmail.com>
2022-11-28 17:27:30 +0200

    push: 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+function srt(b, c) {
+	return (b - c);
+}
+
+let ints = process.argv;
+if (ints.length === 2 || ints.length === 3) {
+	console.log(0);
+} else {
+	ints = ints.slice(2);
+	ints.forEach( (ele, i) => {
+		ints[i] = parseInt(ele);
+	});
+	ints.sort(srt);
+	console.log(ints[ints.length - 2]);
+}
