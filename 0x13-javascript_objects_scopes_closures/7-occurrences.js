f983c86f7f9c0b5b3370ae23921c5acc83d7f42a
SifisoSithole <sitholesifisobrian@gmail.com>
2022-11-29 14:34:37 +0200

    push: 1 - 4

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+	let count = 0;
+	list.forEach( (v) => {
+		if (v === searchElement) {
+			count++;
+		}
+	});
+	return (count);
+}
