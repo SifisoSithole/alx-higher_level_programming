bfaacdbb125cf141829222c6e9147d501e520d7a
SifisoSithole <sitholesifisobrian@gmail.com>
2022-12-02 12:41:15 +0200

    push

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""This script prints the first State object from the database
+hbtn_0e_6_usa"""
+
+from sys import argv as det
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from model_state import Base, State
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(det[1],
+                           det[2], det[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    result = session.query(State).order_by(State.id).first()
+    if result is None:
+        print()
+    else:
+        print(f"{result.id}: {result.name}")
