3c9cbaa3e5b9845735d8934a46eab63b1851e78e
SifisoSithole <sitholesifisobrian@gmail.com>
2022-12-02 11:30:36 +0200

    push

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+Creates a State model
+
+Linked to a database
+"""
+
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Represents a state in thye mysql database
+    Parameters:
+        __tablename__ (str): Name of the MySQL table
+        id (int): The state's id
+        name (str): The state's name
+    """
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128), nullable=False)
