499f50d853bffea277b12feaa697123ba28cd40e
SifisoSithole <sitholesifisobrian@gmail.com>
2022-12-02 12:05:32 +0200

    push

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""lists all State objects from the database hbtn_0e_6_usa
+"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+det = sys.argv[1:]
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(det[0],
+                           det[1], det[2]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    for state in session.query(State).order_by(State.id).all():
+        print(f"{state.id}: {state.name}")
