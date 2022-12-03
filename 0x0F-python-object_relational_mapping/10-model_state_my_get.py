dd6396ff941fcd587095fa63f34b8dcc82e621d6
SifisoSithole <sitholesifisobrian@gmail.com>
2022-12-03 10:29:49 +0200

    push

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -13,14 +13,14 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from model_state import Base, State
 
-engine = create_engine(f'mysql+mysqldb://{det[1]}:{det[2]}@localhost/{det[3]}')
-Base.metadata.create_all(engine)
-
-session = Session(engine)
-result = session.query(State).filter(State.name == det[4]).first()
-
-if result is None:
-    print("Not found")
-else:
-    print(result.id)
-session.close()
+if __name__ == '__main__':
+    string = f'mysql+mysqldb://{det[1]}:{det[2]}@localhost/{det[3]}'
+    engine = create_engine(string)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+    result = session.query(State).filter(State.name == det[4]).first()
+    if result is None:
+        print("Not found")
+    else:
+        print(result.id)
+    session.close()
