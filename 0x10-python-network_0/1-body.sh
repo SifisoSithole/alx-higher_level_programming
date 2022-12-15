0972b41073a207d0cbb34a8c35121d8b344a8d1f
SifisoSithole <sitholesifisobrian@gmail.com>
2022-12-15 12:55:30 +0200

    push

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
+ST=$(curl -Is "$1" | head -1 | cut -d ' ' -f2)
+if [[ $ST -eq 200 ]]
+then
+	curl -s "$1"
+fi
