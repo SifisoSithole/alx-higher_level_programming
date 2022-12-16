f6f6cee6ff0a9d761cfde63e7f7e3f08282a0870
SifisoSithole <sitholesifisobrian@gmail.com>
2022-12-16 14:50:58 +0200

    push

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# This script fetches https://alx-intranet.hbtn.io/status
+"""This script fetches https://alx-intranet.hbtn.io/status"""
 
 if __name__ == "__main__":
     import urllib.request
@@ -9,4 +9,3 @@ if __name__ == "__main__":
         print(f'\t- type: {type(html)}')
         print(f'\t- content: {html}')
         print(f'\t- utf8 content: {html.decode("utf-8")}')
-
