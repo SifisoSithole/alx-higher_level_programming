0f8e68fac87dc255c91c4cab6c9ec9435a49844f
SifisoSithole <sitholesifisobrian@gmail.com>
2022-12-16 19:23:14 +0200

    push

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL, sends a request to the URL and
+displays the body of the response.
+"""
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv as p
+
+    r = requests.get(p[1])
+    try:
+        if r.status_code == requests.codes.ok:
+            print(r.text)
+        else:
+            print(r.status_code)
+    except Exception:
+        print('error')
