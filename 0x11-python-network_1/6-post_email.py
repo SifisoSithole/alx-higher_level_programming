c47b0f51a418c4d62441fed53e2d8152e415e9a3
SifisoSithole <sitholesifisobrian@gmail.com>
2022-12-16 19:07:13 +0200

    push

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Python script that takes in a URL and an email address, sends a POST
+request to the passed URL with the email as a parameter, and finally
+displays the body of the response.
+"""
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv as p
+    res = requests.post(p[1], {'email': p[2]})
+    print(res.text)
