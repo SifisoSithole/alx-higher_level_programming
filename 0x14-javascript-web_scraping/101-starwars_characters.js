0baa4e7a9f04567e2b1124016012ea872b9e4e46
SifisoSithole <sitholesifisobrian@gmail.com>
2023-01-17 17:59:55 +0200

    push

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,22 +1,28 @@
 #!/usr/bin/node
-// This script that prints all characters of a Star Wars movie
+// A script that prints all characters of a Star Wars movie
+// Display characters name in the same order of the list  “characters” in the /films/ response
 
-const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-request.get(url, (err, resp, body) => {
-  if (!err) {
-    const characters = JSON.parse(body).characters;
-    for (const i in characters) {
-      const url2 = characters[i].substring(0, characters[i].length - 1);
-      request.get(url2, (e, res, b) => {
-        if (!e) {
-          while (!res && res.statusCode !== 200) {
-            continue;
-          }
-          console.log(JSON.parse(b).name);
-        }
-      });
+function printCharacters (characters, idx) {
+  request(characters[idx], (err, res, body) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(JSON.parse(body).name);
+      if (idx + 1 < characters.length) {
+        printCharacters(characters, idx + 1);
+      }
     }
+  });
+}
+
+request(url, (err, res, body) => {
+  if (err) {
+    console.log(err);
+  } else {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
