d65f23b24f4fb0954f3747b97fc82ad2ae99668f
SifisoSithole <sitholesifisobrian@gmail.com>
2023-01-17 13:12:03 +0200

    push: files

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// This script that prints all characters of a Star Wars movie
+
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+const request = require('request');
+
+request.get(url, (err, resp, body) => {
+  if (!err) {
+    const characters = JSON.parse(body).characters;
+    for (const i in characters) {
+      const url2 = characters[i].substring(0, characters[i].length - 1);
+      request.get(url2, (e, res, b) => {
+        if (!e) {
+          console.log(JSON.parse(b).name);
+        }
+      });
+    }
+  }
+});
