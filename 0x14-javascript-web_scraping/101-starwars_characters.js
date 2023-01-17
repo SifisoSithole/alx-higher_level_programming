ab23faa8c6bebeb9802ddadfa4ec32b7be90076b
SifisoSithole <sitholesifisobrian@gmail.com>
2023-01-17 17:36:23 +0200

    push

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -11,6 +11,9 @@ request.get(url, (err, resp, body) => {
       const url2 = characters[i].substring(0, characters[i].length - 1);
       request.get(url2, (e, res, b) => {
         if (!e) {
+          while (res.statusCode !== 200) {
+            continue;
+          }
           console.log(JSON.parse(b).name);
         }
       });
